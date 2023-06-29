da5e0f80435184c84855914cc8c083e4dada4cf3
HarishPJ21 <38295998+HarishPJ21@users.noreply.github.com>
2023-06-29 23:25:32 +0530

    UI

--- src/index.js
+++ src/index.js
@@ -5,10 +5,28 @@ import { Tooltip } from './Tooltip';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <React>
     <Tooltip text={"tooltip message for you"}>
       <button>Tooltip</button>
     </Tooltip>
-  </React.StrictMode>
+  </React>
 );
 
+// <button>
+// Write, edit and run HTML, CSS and JavaScript code online.
+// </button>
+// <div class="tooltip-top">  
+// <span class="tip tip-up"></span>
+// <p >
+// Our HTML editor updates the webview automatically in real-time as you write code.
+// </p>
+// </div>
+// <p class="tooltip-right">
+// Our HTML editor updates the webview automatically in real-time as you write code.
+// </p>
+// <p class="tooltip-left">
+// Our HTML editor updates the webview automatically in real-time as you write code.
+// </p>
+// <p class="tooltip-bottom">
+// Our HTML editor updates the webview automatically in real-time as you write code.
+// </p>
