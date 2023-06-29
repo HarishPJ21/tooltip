46df8f2a5b560e3e8e31f82ad9f89b46ed12e7c3
HarishPJ <jayaharishpilladi@gmail.com>
2023-06-30 00:51:47 +0530

    complete code

--- src/Tooltip.jsx
+++ src/Tooltip.jsx
@@ -2,7 +2,17 @@ import React, { useState } from "react";
 
 export const Tooltip = ({text,children}) => {
   const [isVisible, setIsVisible] = useState(false);
+  const [direction,setDirection] = useState(); 
   return (
+    <>
+    <div className="dropDown">
+      <select value={direction} onChange={e=>setDirection(e.target.value)}>
+      <option>left</option>
+      <option>right</option>
+      <option>top</option>
+      <option>bottom</option>
+      </select>
+    </div>
     <div
       className="tooltip"
       onMouseEnter={() => setIsVisible(true)}
@@ -10,7 +20,8 @@ export const Tooltip = ({text,children}) => {
     >
         {children}
         {console.log(isVisible)}
-      {isVisible && <div className="tooltip-box"> {text} </div>}  
+      {isVisible && <div className={`tooltip-${direction}`}><span className={`tip tip-${direction}`}></span> {text} </div>}  
     </div>
+    </>
   );
 };
--- src/index.js
+++ src/index.js
@@ -5,28 +5,10 @@ import { Tooltip } from './Tooltip';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React>
+  <React.StrictMode>
     <Tooltip text={"tooltip message for you"}>
-      <button>Tooltip</button>
+      <button>Hover over me</button>
     </Tooltip>
-  </React>
+  </React.StrictMode>
 );
 
-// <button>
-// Write, edit and run HTML, CSS and JavaScript code online.
-// </button>
-// <div class="tooltip-top">  
-// <span class="tip tip-up"></span>
-// <p >
-// Our HTML editor updates the webview automatically in real-time as you write code.
-// </p>
-// </div>
-// <p class="tooltip-right">
-// Our HTML editor updates the webview automatically in real-time as you write code.
-// </p>
-// <p class="tooltip-left">
-// Our HTML editor updates the webview automatically in real-time as you write code.
-// </p>
-// <p class="tooltip-bottom">
-// Our HTML editor updates the webview automatically in real-time as you write code.
-// </p>
