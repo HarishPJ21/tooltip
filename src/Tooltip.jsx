5d74e423facf8a2a984994c22f883dea288bb21c
HarishPJ21 <38295998+HarishPJ21@users.noreply.github.com>
2023-08-09 12:17:46 +0000

    commented code

--- src/Tooltip.jsx
+++ src/Tooltip.jsx
@@ -2,10 +2,11 @@ import React, { useState } from "react";
 
 export const Tooltip = ({text,children}) => {
   const [isVisible, setIsVisible] = useState(false);
-  const [direction,setDirection] = useState(); 
+  const [direction,setDirection] = useState("left"); 
   return (
     <>
     <div className="dropDown">
+      {/* based on the option selected the direction will be set using hooks */}
       <select value={direction} onChange={e=>setDirection(e.target.value)}>
       <option>left</option>
       <option>right</option>
@@ -15,11 +16,14 @@ export const Tooltip = ({text,children}) => {
     </div>
     <div
       className="tooltip"
+      //based on the hover the isVisible will be set to true using the hook
       onMouseEnter={() => setIsVisible(true)}
       onMouseLeave={() => setIsVisible(false)}
     >
         {children}
-        {console.log(isVisible)}
+        {/* {console.log(isVisible)} */}
+        {/*1. only when is visible is true the below div will be shown 
+           2. class name inside the tool tip will change dynamically with respect to the option selected in direction */}
       {isVisible && <div className={`tooltip-${direction}`}><span className={`tip tip-${direction}`}></span> {text} </div>}  
     </div>
     </>
--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import { Tooltip } from './Tooltip';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    {/* passing text to be shown in the tooltip as props */}
     <Tooltip text={"tooltip message for you"}>
       <button>Hover</button>
     </Tooltip>
